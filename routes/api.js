e6dfd353c3c68988c823a3bf0043d1fba5de5ca3
Helaldm <helenmillatruz17@gmail.com>
2025-06-25 11:02:46 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,35 @@
-const express = require('express');
-const convertHandler = require('../controllers/convertHandler');
-const router = express.Router();
+"use strict";
 
-router.get('/convert', (req, res) => {
-  const input = req.query.input;
-  const initNum = convertHandler.getNum(input);
-  const initUnit = convertHandler.getUnit(input);
-  
-  if (initUnit === 'invalid unit') {
-    return res.send({ error: 'invalid unit' });
-  }
-  
-  const returnNum = convertHandler.convert(initNum, initUnit);
-  const returnUnit = initUnit === 'L' ? 'gal' : initUnit === 'gal' ? 'L' : 
-                     initUnit === 'kg' ? 'lbs' : 'kg';
-  
-  const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-  res.send({ initNum, initUnit, returnNum, returnUnit, string });
-});
+const expect = require("chai").expect;
+const ConvertHandler = require("../controllers/convertHandler.js");
 
-module.exports = router;
+module.exports = function (app) {
+  let convertHandler = new ConvertHandler();
+
+  app.route("/api/convert").get((req, res) => {
+    const { input } = req.query;
+    if (!input) {
+      res.send("invalid input");
+    } else {
+      const initNum = convertHandler.getNum(input);
+      const initUnit = convertHandler.getUnit(input);
+      if (initNum === "invalid number" && initUnit === "invalid unit") {
+        res.send("invalid number and unit");
+      } else if (initNum === "invalid number") {
+        res.send("invalid number");
+      } else if (initUnit === "invalid unit") {
+        res.send("invalid unit");
+      } else {
+        const returnNum = convertHandler.convert(initNum, initUnit);
+        const returnUnit = convertHandler.getReturnUnit(initUnit);
+        const string = convertHandler.getString(
+          initNum,
+          initUnit,
+          returnNum,
+          returnUnit,
+        );
+        res.json({ initNum, initUnit, returnNum, returnUnit, string });
+      }
+    }
+  });
+};
