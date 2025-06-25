a35a00b76cac7652451b84b946c4f489a3c5c417
Helaldm <helenmillatruz17@gmail.com>
2025-06-25 10:45:47 -0300

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,21 @@
-const chai = require('chai');
-let assert = chai.assert;
-const ConvertHandler = require('../controllers/convertHandler.js');
+const convertHandler = require('../controllers/convertHandler');
+const { assert } = require('chai');
 
-let convertHandler = new ConvertHandler();
-
-suite('Unit Tests', function(){
-
+describe('Unit Tests', function() {
+  it('should read a whole number input', function() {
+    assert.equal(convertHandler.getNum('5kg'), 5);
+  });
+  it('should read a decimal input', function() {
+    assert.equal(convertHandler.getNum('5.5kg'), 5.5);
+  });
+  it('should read a fractional input', function() {
+    assert.equal(convertHandler.getNum('1/2kg'), 0.5);
+  });
+  it('should read a fractional input with decimal', function() {
+    assert.equal(convertHandler.getNum('5.2/2kg'), 2.6);
+  });
+  it('should return "invalid unit" for an invalid unit', function() {
+    assert.equal(convertHandler.getUnit('32g'), 'invalid unit');
+  });
+  // Agrega más pruebas según sea necesario...
 });
