374e40a83350a825eb61950ee6d64d387b1563ef
Helaldm <helenmillatruz17@gmail.com>
2025-06-25 10:44:51 -0300

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,22 @@
-'use strict';
+const express = require('express');
+const convertHandler = require('../controllers/convertHandler');
+const router = express.Router();
 
-const expect = require('chai').expect;
-const ConvertHandler = require('../controllers/convertHandler.js');
-
-module.exports = function (app) {
+router.get('/convert', (req, res) => {
+  const input = req.query.input;
+  const initNum = convertHandler.getNum(input);
+  const initUnit = convertHandler.getUnit(input);
   
-  let convertHandler = new ConvertHandler();
+  if (initUnit === 'invalid unit') {
+    return res.send({ error: 'invalid unit' });
+  }
+  
+  const returnNum = convertHandler.convert(initNum, initUnit);
+  const returnUnit = initUnit === 'L' ? 'gal' : initUnit === 'gal' ? 'L' : 
+                     initUnit === 'kg' ? 'lbs' : 'kg';
+  
+  const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
+  res.send({ initNum, initUnit, returnNum, returnUnit, string });
+});
 
-};
+module.exports = router;
