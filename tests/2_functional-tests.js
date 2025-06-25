89909ec1ea5c29c657b72d95bbaa9c2d83c4f5f0
Helaldm <helenmillatruz17@gmail.com>
2025-06-25 10:46:12 -0300

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,10 +1,28 @@
-const chaiHttp = require('chai-http');
 const chai = require('chai');
-let assert = chai.assert;
-const server = require('../server');
+const chaiHttp = require('chai-http');
+const server = require('../server'); // Ajusta la ruta según sea necesario
 
 chai.use(chaiHttp);
+const { expect } = chai;
 
-suite('Functional Tests', function() {
-
+describe('Functional Tests', function() {
+  it('should convert a valid input like 10L', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=10L')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('string');
+        done();
+      });
+  });
+  it('should return "invalid unit" for an invalid input like 32g', function(done) {
+    chai.request(server)
+      .get('/api/convert?input=32g')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        expect(res.body).to.have.property('error').eql('invalid unit');
+        done();
+      });
+  });
+  // Agrega más pruebas según sea necesario...
 });
