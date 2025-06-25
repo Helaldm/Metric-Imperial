1a5f239a832e2e6b52a5940baba21a037723f518
Helaldm <helenmillatruz17@gmail.com>
2025-06-25 10:44:02 -0300

    Update convertHandler.js

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,33 @@
-function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
+const convertHandler = {
+  getNum: function(input) {
+    const result = input.match(/(\d+\.?\d*\/\d+\.?\d*|\d+\.?\d*|\d+)/);
+    if (result) return eval(result[0]);
+    return 1; // Valor predeterminado
+  },
+  getUnit: function(input) {
+    const result = input.match(/[a-zA-Z]+/);
+    const unit = result ? result[0].toLowerCase() : '';
+    const validUnits = ['gal', 'l', 'mi', 'km', 'lbs', 'kg'];
     
-    return result;
-  };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
-    return result;
-  };
+    if (validUnits.includes(unit)) {
+      return unit === 'l' ? 'L' : unit;
+    }
+    return 'invalid unit';
+  },
+  convert: function(num, unit) {
+    const conversions = {
+      gal: num * 3.78541,
+      l: num / 3.78541,
+      mi: num * 1.60934,
+      km: num / 1.60934,
+      lbs: num * 0.453592,
+      kg: num / 0.453592,
+    };
+    return conversions[unit];
+  },
+  getString: function(initNum, initUnit, returnNum, returnUnit) {
+    return `${initNum} ${initUnit} converts to ${returnNum} ${returnUnit}`;
+  }
+};
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
-  };
-  
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
-    
-    return result;
-  };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
-  };
-  
-}
-
-module.exports = ConvertHandler;
+module.exports = convertHandler;
